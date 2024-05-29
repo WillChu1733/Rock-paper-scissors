cdce69a8304ccf0a44597ad1c64f689017f5eb7d
William Chu <willchu1733@gmail.com>
2024-05-29 13:43:00 +1200

    update: getComputerChoice so random number gen is used as index for array

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-// computer choice picks random number 1-3
-// each number represents rock, paper or scissors
-const getComputerChoice = () => Math.floor(Math.random() * 3) + 1
-
-console.log(getComputerChoice())
-
-function getHumanChoice() {
-  let choice = prompt('Choose your symbol', 'test') // open the prompt
-
-  if (choice == 1) {
-    console.log('you chose 1')
-  } else if (choice == 2) {
-    console.log('you chose 2')
-  } else if (choice == 3) {
-    console.log('you chose 3')
-  } else return console.log('invalid')
+// computer choice picks random number 0-2
+// random number is used as an index to select and log a string from the arrayOfChoices array
+const getComputerChoice = () => {
+  const randomNum = Math.floor(Math.random() * 3)
+  console.log(randomNum)
+  const arrayOfChoices = ['Rock', 'Paper', 'Scissors']
+  console.log(arrayOfChoices[randomNum])
 }
 
-getHumanChoice()
+getComputerChoice()
 
-//initialise scores of both players starting at 0
-const humanScore = 0
-const computerScore = 0
+// function getHumanChoice() {
+//   let choice = prompt('Choose your symbol', 'test') // open the prompt
+
+//   if (choice == 1) {
+//     console.log('you chose 1')
+//   } else if (choice == 2) {
+//     console.log('you chose 2')
+//   } else if (choice == 3) {
+//     console.log('you chose 3')
+//   } else return console.log('invalid')
+// }
+
+// getHumanChoice()
+
+// //initialise scores of both players starting at 0
+// const humanScore = 0
+// const computerScore = 0
+
+// function playRound(humanChoice, computerChoice) {}
+
+// const humanSelection = getHumanChoice()
+// const computerSelection = getComputerChoice()
+
+// playRound(humanSelection, computerSelection)
